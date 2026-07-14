5176d614d91c71ca065fc06afdb8adf3970bf8f7
lukasbrugger <luk.brugger@gmail.com>
2026-07-14 19:11:32 +0200

    chore(plotting): Importe und Logger vorbereiten

---
+++ ebike_sim/plotting.py
@@ -0,0 +1,8 @@
+"""Plot- und Kartenfunktionen für das Abschlussprojekt."""
+from pathlib import Path
+import logging
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+LOGGER = logging.getLogger(__name__)
